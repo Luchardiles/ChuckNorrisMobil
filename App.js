ceb95d00c8a1fdacc2dce618b8050211c9b76c39
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2023-12-07 18:58:02 -0300

    Refactorizar

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import { TextInput } from 'react-native-paper';
 const ChuckNorrisJoke = () => {
   const [joke, setJoke] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
-
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('');
 
+  const baseURL = 'https://api.chucknorris.io/jokes';
+
+  // Fetch categories from Chuck Norris API
   const fetchCategories = async () => {
     try {
-      const response = await fetch('https://api.chucknorris.io/jokes/categories');
+      const response = await fetch(`${baseURL}/categories`);
       const data = await response.json();
       setCategories(data);
     } catch (error) {
@@ -19,11 +21,10 @@ const ChuckNorrisJoke = () => {
     }
   };
 
+  // Fetch random joke from Chuck Norris API
   const fetchChuckNorrisJoke = async () => {
     try {
-      let apiUrl = 'https://api.chucknorris.io/jokes/random';
-
-      const response = await fetch(apiUrl);
+      const response = await fetch(`${baseURL}/random`);
       const data = await response.json();
       setJoke(data.value);
 
@@ -32,32 +33,29 @@ const ChuckNorrisJoke = () => {
     }
   };
 
+  // Fetch random joke with category from Chuck Norris API
   const fetchChuckNorrisJokewithCategory = async () => {
-    try {
-      let apiUrl = 'https://api.chucknorris.io/jokes/random';
-      if (selectedCategory) {
-        apiUrl += `?category=${selectedCategory}`;
-      }
-      
-      const response = await fetch(apiUrl);
+    try {      
+      const response = await fetch(`${baseURL}/random?category=${selectedCategory}`);
       const data = await response.json();
       setJoke(data.value);
-
     } catch (error) {
       console.error('Error fetching Chuck Norris joke:', error);
     }
   };
 
+  // Search joke with text from Chuck Norris API
   const fetchChuckNorrisJokewithText = async () => {
     try {
-      let apiUrl = 'https://api.chucknorris.io/jokes/random';
-      if (selectedCategory) {
-        apiUrl += `?category=${selectedCategory}`;
-      }
-      
-      const response = await fetch(apiUrl);
+      const response = await fetch(`${baseURL}/search?query=${searchQuery}`);
       const data = await response.json();
-      setJoke(data.value);
+      
+      if (data.result.length > 0) {
+        const randomIndex = Math.floor(Math.random() * data.result.length);
+        setJoke(data.result[randomIndex].value);
+      } else {
+        setJoke(joke);
+      }
 
     } catch (error) {
       console.error('Error fetching Chuck Norris joke:', error);
@@ -76,7 +74,7 @@ const ChuckNorrisJoke = () => {
         value={searchQuery}
         onChangeText={(text) => setSearchQuery(text)}
       />
-      <Button title="Get me a new one" onPress={fetchChuckNorrisJoke} />
+      <Button title="Buscar chiste" onPress={fetchChuckNorrisJokewithText} />
       <Text>{joke}</Text>
       <Button title="Dame un chiste aleatorio" onPress={fetchChuckNorrisJoke} />
       <Picker
